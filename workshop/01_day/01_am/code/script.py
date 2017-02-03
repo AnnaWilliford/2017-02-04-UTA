8c13088adc73c8e16eba2896e810deed970faa92
devendra1810 <devendra1810@gmail.com>
2017-02-02 23:56:22 -0600

    updated script.py

--- workshop/01_day/01_am/code/script.py
+++ workshop/01_day/01_am/code/script.py
@@ -1,7 +1,18 @@
+import sys
 import pandas
-dem_data = pandas.read_csv("C:\Users\Devendra\Desktop\Software_carpentry_Feb_2017\Day 1\python\data\Dem_HealthData\TX.txt" , sep = '\t')
-plt.scatter(dem_data['Population_Density'],dem_data['Poverty'])
-plt.xlabel('Population Density')
-plt.ylabel('Poverty')
-plt.title('Relation between Population Density vs Poverty')
-plt.show()
+import matplotlib.pyplot
+
+file_name=sys.argv[1]
+col1=sys.argv[2]
+col2=sys.argv[3]
+
+print col1
+print col2
+print file_name
+
+
+dem_data = pandas.read_csv(file_name , sep = '\t')
+matplotlib.pyplot.scatter(dem_data[col1],dem_data[col2])
+matplotlib.pyplot.xlabel(col1)
+matplotlib.pyplot.ylabel(col2)
+matplotlib.pyplot.show()
